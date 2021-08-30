e85cc142c675dc9c21f6763f1a0786be05b169df
MischaJoosten <MischaJoosten@users.noreply.github.com>
2021-08-30 16:06:18 +0100

    Array 2

---
+++ Arrays/Opdracht2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Opdracht2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[3, 3]
+            {
+                { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }
+            };
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[ i, j ]);
+                }
+            }
+        }
+    }
+}
