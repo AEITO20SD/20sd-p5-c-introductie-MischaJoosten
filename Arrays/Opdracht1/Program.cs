075994a1032e8a0dfc6ba9ce0bde336bcf15391d
MischaJoosten <MischaJoosten@users.noreply.github.com>
2021-08-30 15:59:57 +0100

    Array 1

---
+++ Arrays/Opdracht1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Opdracht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] zin = new string[3];
+
+            string[] zin2 = { "Peter", "is", "de", "broer", "van", "Hans" };
+
+            
+            foreach (var woord in zin2)
+            {
+                Console.Write(woord + " ");
+            }
+            Console.WriteLine();
+            zin2[0] = "Hans";
+            zin2[5] = "Peter";
+            foreach (var woord2 in zin2)
+            {
+
+                Console.Write(woord2 + " ");
+            }
+        }
+    }
+}
