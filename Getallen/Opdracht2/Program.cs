d34d42d041a3382ccf7c50fc5b49819cb5ba77be
MischaJoosten <MischaJoosten@users.noreply.github.com>
2021-09-03 10:45:00 +0100

    Methods 2

---
+++ Getallen/Opdracht2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Opdracht2
+{
+    class Program
+    {
+        private static int Leesgeheelgetal()
+        {
+            bool geldigGetal;
+            int getal;
+            do
+            {
+                string input = Console.ReadLine();
+                geldigGetal = int.TryParse(input, out getal);
+                if (!geldigGetal)
+                {
+                    Console.WriteLine("Voer een geldig getal in");
+                }
+            } while (!geldigGetal);
+            return getal;
+        }
+        private static string GeefKaart(int getal)
+        {
+            string kaart;
+            switch (getal)
+            {
+                case 1:
+                    kaart = "ruiten";
+                    break;
+                case 2:
+                    kaart = "harten";
+                    break;
+                case 3:
+                    kaart = "klaver";
+                    break;
+                default:
+                    kaart = "schoppen";
+                    break;
+            }
+            return kaart;
+        }
+        static void Main(string[] args)
+        {
+            int getal = 0;
+            do
+            {
+                Console.WriteLine("Kies een getal tussen 1 en 4");
+                getal = Leesgeheelgetal();
+            } while (getal < 1 || getal > 4);
+            string kaart = GeefKaart(getal);
+            Console.WriteLine($"U heeft {getal} gekozen. Uw kaart is {kaart}");
+            Console.ReadLine();
+        }
+    }
+}
