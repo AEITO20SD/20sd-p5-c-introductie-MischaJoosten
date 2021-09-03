61cf7112bc6ddc3abbd84c14075c08237df5404e
MischaJoosten <MischaJoosten@users.noreply.github.com>
2021-09-03 09:41:25 +0100

    Heb commentaar toegevoegd

--- Arrays/Opdracht1/Program.cs
+++ Arrays/Opdracht1/Program.cs
@@ -6,22 +6,19 @@ namespace Opdracht1
     {
         static void Main(string[] args)
         {
-            string[] zin = new string[3];
-
-            string[] zin2 = { "Peter", "is", "de", "broer", "van", "Hans" };
+            string[] zin2 = { "Peter", "is", "de", "broer", "van", "Hans" }; //Zet Peter is de broer van Hans in een string genaamt zin2
 
             
-            foreach (var woord in zin2)
+            foreach (var woord in zin2) // Veranderd in deze foreach zin2 naar woord en kan alleen hier worden gebruikt
             {
-                Console.Write(woord + " ");
+                Console.Write(woord + " "); // Print de woorden bij zin2 plus een spatie tussen de woorden
             }
-            Console.WriteLine();
-            zin2[0] = "Hans";
-            zin2[5] = "Peter";
-            foreach (var woord2 in zin2)
+            Console.WriteLine(); // Voegt een enter toe aan de tekst 
+            zin2[0] = "Hans"; // Veranderd Peter in Hans
+            zin2[5] = "Peter"; // Veranders Hans is Peter
+            foreach (var woord2 in zin2) // Veranderd in deze foreach zin2 naar woord2 en kan alleen hier worden bebruikt
             {
-
-                Console.Write(woord2 + " ");
+                Console.Write(woord2 + " "); // Print de woorden van zin2 maar dan wel met Hans en Peter omgedraait.
             }
         }
     }
--- Arrays/Opdracht2/Program.cs
+++ Arrays/Opdracht2/Program.cs
@@ -8,26 +8,25 @@ namespace Opdracht2
         static void Main(string[] args)
         {
 
-            int[,] array = new int[3, 3]
+            int[,] array = new int[3, 3] // maakt deze integer aan en checkt of het wel 3 bij 3 is
             {
                 { 1, 2, 3 },
                 { 4, 5, 6 },
                 { 7, 8, 9 }
             };
 
-            for (int rij = 0; rij < 3; rij++)
+            for (int rij = 0; rij < 3; rij++) // zorgt voor boven naar beneden voor de lengte
             {
-                for (int column = 0; column < 3; column++)
+                for (int column = 0; column < 3; column++) // zorgt voor links naar rechts voor de lengte
                 {
-                    Console.Write(array[rij, column] + " ");
+                    Console.Write(array[rij, column] + " "); // Print de colomn uit
                 }
-                Console.WriteLine();
+                Console.WriteLine(); // Voegt die enter toe
             }
 
 
-            //telt alles bij elkaar op
-            int[] Optellen = { 1, 2, 3, 4, 5, 6, 7, 8, 9, };
-            Console.WriteLine(Optellen.Sum());
+            int[] Optellen = { 1, 2, 3, 4, 5, 6, 7, 8, 9, }; // Telt alles bij elkaar op
+            Console.WriteLine(Optellen.Sum()); // Print de opgetelde tekst
         }
     }
 }
