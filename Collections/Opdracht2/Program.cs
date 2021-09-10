f7acda5b2767689dd7187bd1171adc1f7aba44e2
MischaJoosten <MischaJoosten@users.noreply.github.com>
2021-09-10 09:05:46 +0100

    Collections

--- Collections/Opdracht2/Program.cs
+++ Collections/Opdracht2/Program.cs
@@ -19,6 +19,7 @@ namespace Opdracht2
 
         private static string HaalKeuze()
         {
+            // Vraagt en leest welke keuze je maakt
             Console.WriteLine("Vul een keuze in");
             Console.WriteLine("Stoppen, Toevoegen, Weghalen");
             var keuze = Console.ReadLine();
@@ -37,16 +38,19 @@ namespace Opdracht2
 
         private static string Weghalen(Queue<string> file)
         {
+            // Hier wordt voorste voertuig verwijderd
             file.Dequeue();
             return "Weggehaald";
         }
 
         private static string Stoppen(Queue<string> file)
         {
+            // Hier stopt de app
             return "Stopt";
         }
         private static string Printen(Queue<string> file)
         {
+            // Print welke voertuigen in de queue zitten en hoeveel er in zitten
             foreach (var voertuig in file)
             {
                 Console.Write($"[{voertuig}]" + " ");
@@ -55,7 +59,7 @@ namespace Opdracht2
             Console.WriteLine($"Er staan {file.Count} voertuigen in de file");
             Console.WriteLine();
             
-            return "printen";
+            return "";
         }
         static void Main(string[] args)
         {
