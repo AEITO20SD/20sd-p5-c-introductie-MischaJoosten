904046db55a15dbc4401ff954d43e5f788f93803
MischaJoosten <MischaJoosten@users.noreply.github.com>
2021-09-10 09:05:37 +0100

    OOP 1

---
+++ OOP/Opdracht1/Persoon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Personen
+{
+    public class Persoon
+    {
+        public string Voornaam { get; set; }
+
+        public string Achternaam { get; set; }
+
+        public DateTime GeboorteDatum { get; set; }
+
+        public string Geslacht { get; set; }
+
+        public int Lengte { get; set; }
+    }
+}
