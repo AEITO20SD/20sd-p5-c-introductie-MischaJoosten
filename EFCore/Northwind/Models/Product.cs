e2e6579492a859bba030f7d1c0ed488c482d60b0
MischaJoosten <MischaJoosten@users.noreply.github.com>
2021-09-20 14:14:37 +0100

    EFCore 2

---
+++ EFCore/Northwind/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Northwind.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required, StringLength(40)]
+        public string ProductName { get; set; }
+
+        [ForeignKey("Categories")]
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+
+        [Required, StringLength(20)]
+        public string QuantityPerUnit { get; set; }
+
+        [Required]
+        public double UnitsPrice { get; set; }
+
+        [Column(TypeName = "smallint")]
+        public int UnitsInStock { get; set; }
+    }   
+}
