d16c5d8787b31f3aad460e1156e1e71fecaf7518
MischaJoosten <MischaJoosten@users.noreply.github.com>
2021-09-20 14:21:55 +0100

    EFCore 3

---
+++ EFCore/Northwind/Data/NorthwindDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Northwind.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Data
+{
+    class NorthwindDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        private string _connectionString;
+
+        public NorthwindDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
