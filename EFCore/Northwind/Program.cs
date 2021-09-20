d61c8667bcda91632cc69e7bf23ae2cee59efec1
MischaJoosten <MischaJoosten@users.noreply.github.com>
2021-09-20 14:27:23 +0100

    EFCore 4

--- EFCore/Northwind/Program.cs
+++ EFCore/Northwind/Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Northwind.Data;
+using System;
+using System.Linq;
 
 namespace Northwind
 {
     class Program
     {
+        private static string _connectionsString = @"Server=(localdb)\MSSQLLocalDB;Database=Northwind";
+
+        private static void ToonCategories()
+        {
+            var db = new NorthwindDbContext(_connectionsString);
+            var categories = db.Categories.ToList();
+            foreach (var category in categories)
+            {
+                Console.WriteLine($"{category.CategoryID}: ");
+                Console.WriteLine(category.CategoryName);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ToonCategories();
         }
     }
 }
