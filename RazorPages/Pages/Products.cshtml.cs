8c6a1169f869f6072557e4628af868f72297ebd7
MischaJoosten <MischaJoosten@users.noreply.github.com>
2021-09-27 12:54:43 +0100

    RazorPages Opdracht 2

---
+++ RazorPages/Pages/Products.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Northwind.Data;
+using Northwind.Models;
+
+namespace RazorPages.Pages
+{
+    public class ProductsModel : PageModel
+    {
+        private readonly NorthwindDbContext _context;
+
+        public List<Product> Products { get; set; }
+        public List<Category> Categories { get; private set; }
+
+        public ProductsModel(NorthwindDbContext context)
+        {
+            _context = context;
+        }
+        public void OnGet()
+        {
+            ViewData["Title"] = "Products";
+
+            Products = _context.Products
+                .OrderBy(Product => Product.ProductName)
+                .ToList();
+            Categories = _context.Categories.ToList();
+        }
+    }
+}
